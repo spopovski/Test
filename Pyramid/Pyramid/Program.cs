92d4b39c19a5590f61391b665550fd2d84d0ba3e
spopovski <s.popovski96@gmail.com>
2018-03-19 15:40:26 +0200

    Create

---
+++ Pyramid/Pyramid/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sum13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num, i, j, k;
+            Console.Write("enter the level:");
+            num = Convert.ToInt32(Console.ReadLine());
+            for (i = 1; i <= num; i++)
+            {
+                for (j = 1; j < num - i + 1; j++)
+                {
+                    Console.Write(" ");
+                }
+                for (k = 1; k <= i; k++)
+                {
+                    Console.Write("*");
+                    Console.Write(" ");
+                }
+                Console.WriteLine();
+
+            }
+        }
+    }
+}
